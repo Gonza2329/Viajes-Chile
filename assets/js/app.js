14042aa63a6a930d4e0970b0c6e20bbb6f7d0a7b
Gonzalo Oyanedel <gonzo023@gmail.com>
2022-09-30 13:26:00 -0300

    Se agregan comentarios al proyecto

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,3 +12,15 @@ $(function() {
         $(".card-text").toggle();
     });
 });
+
+//Js para ocultar el navbar cuando se hace scroll hacia abajo
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
